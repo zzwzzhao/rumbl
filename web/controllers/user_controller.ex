82642130450a36543f0a0cad5c0dbb74940a12b0
zhaotianjun <zzwzzhao@163.com>
2016-08-12 00:14:33 +0800

    init

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,19 @@
+defmodule Rumbl.UserController do
+  use Rumbl.Web, :controller
+  alias Rumbl.User
+
+  def index(conn, _params) do
+    users = Repo.all(Rumbl.User)
+    render conn, "index.html", users: users
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Rumbl.User, id)
+    render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+end
