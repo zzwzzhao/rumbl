c33773e7882d6fab3ca1bba5e829f66f0b77c8fb
zhaotianjun <zhaotianjun@xingtuwuxian.com>
2016-08-16 12:04:10 +0800

    watch

--- web/router.ex
+++ web/router.ex
@@ -20,11 +20,12 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    get "/watch/:id", WatchController, :show
   end
 
   scope "/manage", Rumbl do
     pipe_through [:browser, :authenticate_user]
-    
+
     resources "/videos", VideoController
   end
 
